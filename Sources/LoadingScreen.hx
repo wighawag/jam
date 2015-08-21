d01d144f60d527132a35586c3281e03ca0e3202a
Wighawag <wighawag@gmail.com>
2015-08-21 23:51:10 +0100

    make font working + remove unused assets

--- Sources/LoadingScreen.hx
+++ Sources/LoadingScreen.hx
@@ -16,14 +16,11 @@ class LoadingScreen implements Screen {
 
 	var finishedLoading : Bool = false;
 
-	var font : kha.Font;
-
 	public function new() {
 	}
 
   public function enter(input : Input) {
 		Loader.the.loadRoom("loading", function(){
-			font = Loader.the.loadFont("Arial", new FontStyle(false, false, false), 14);
 		});
 		//font = Loader.the.loadFont("Arial", new FontStyle(false, false, false), 14);
     //Configuration.setScreen(new LoadingScreen());
@@ -50,11 +47,9 @@ class LoadingScreen implements Screen {
 			g2.clear();
 			g2.color = Color.Green;
 			g2.drawLine(0,0,frame.width,frame.height);
-			if(font != null){
-					g2.font = font;
-					g2.drawString("test",100,100);//frame.width/2, frame.height/2);
-			}
 
+			g2.font = Loader.the.loadFont("Arial", new FontStyle(false, false, false), 24);
+			g2.drawString("Loading...",frame.width/2,frame.height/2);
 
 		 });
   }
